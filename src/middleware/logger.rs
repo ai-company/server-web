8780228ca7b75e45efe6df71aea7c64062e373d5
Daniels Kursits <daniels.kursits@gmail.com>
2021-05-24 18:19:57 +0300

    clean up and fix middlewares

---
+++ src/middleware/logger.rs
@@ -0,0 +1,52 @@
+use std::future::Future;
+
+use actix_web::dev::{Service, ServiceRequest, ServiceResponse};
+use chrono::Utc;
+
+pub fn logger(
+    req: ServiceRequest,
+    srv: &mut impl Service<
+        Request = ServiceRequest,
+        Response = ServiceResponse,
+        Error = actix_web::Error,
+    >,
+) -> impl Future<Output = Result<ServiceResponse, actix_web::Error>> {
+    let start_time = Utc::now().time();
+    let date = Utc::now().format("%d/%m/%y %T").to_string();
+    let method = req.method().as_str().to_owned();
+    let path = req.uri().path().to_owned();
+
+    let fut = srv.call(req);
+
+    async move {
+        let result = fut.await;
+
+        match &result {
+            Ok(response) => {
+                let end_time = Utc::now().time();
+                let time = end_time - start_time;
+
+                let color = if response.status().as_u16() >= 400 {
+                    "\x1b[31m"
+                } else if response.status().as_u16() == 304 {
+                    ""
+                } else {
+                    "\x1b[32m"
+                };
+
+                println!(
+                    "{}{} {:>6}µs {:>7} {} {:.128}\x1b[0m",
+                    color,
+                    date,
+                    time.num_microseconds().unwrap(),
+                    method,
+                    response.status().as_u16(),
+                    path
+                );
+            }
+            Err(_) => println!("{} \x1b[41m{:7} --- {}\x1b[0m", date, method, path),
+        }
+
+        result
+    }
+}
--- src/route/api/v1/feedback.rs
+++ src/route/api/v1/feedback.rs
@@ -1,24 +1,15 @@
-use actix_web::{client::Client, http::header, web::Data, HttpRequest, HttpResponse, Responder};
+use actix_web::{client::Client, http::header, web::Data, HttpResponse, Responder};
 
-use crate::middleware;
 use crate::route::SharedContext;
 
-pub async fn post(
-    req: HttpRequest,
-    body_string: String,
-    _context: Data<SharedContext>,
-) -> impl Responder {
-    if middleware::is_authorized(&req) {
-        let client = Client::new();
+pub async fn post(body_string: String, _context: Data<SharedContext>) -> impl Responder {
+    let client = Client::new();
 
-        client
-            .post("http://127.0.0.1:6969/api/feedback")
-            .header(header::CONTENT_TYPE, "application/x-www-form-urlencoded")
-            .send_body(body_string)
-            .await;
+    client
+        .post("http://127.0.0.1:6969/api/feedback")
+        .header(header::CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .send_body(body_string)
+        .await;
 
-        HttpResponse::Ok().body("Give me back my feet.")
-    } else {
-        HttpResponse::Ok().body("")
-    }
+    HttpResponse::Ok().body("Give me back my feet.")
 }
