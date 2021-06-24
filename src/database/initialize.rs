570f746814116eb1dbaf380d84c07b9a92310eca
Daniels Kursits <daniels.kursits@gmail.com>
2021-06-24 15:43:44 +0300

    improve migration system

--- src/database/initialize.rs
+++ src/database/initialize.rs
@@ -1,77 +1,76 @@
-use super::{Connection, SqlPool};
-use rusqlite::params;
+use super::SqlPool;
+use rusqlite::{params, Transaction};
 
-const LATEST_SCHEME_VERSION: u32 = 1;
+use super::migration;
 
-pub fn initialize(pool: SqlPool) -> Result<(), String> {
-    if let Ok(con) = pool.get() {
-        // First we get the current database scheme version
+pub type MigrationResult = Result<(), String>;
+pub type Migration = fn(&Transaction) -> MigrationResult;
+
+const SCHEME_MIGRATIONS: [Migration; 1] = [
+    //
+    migration::v001::first_setup,
+];
+
+const SCHEME_VERSION: usize = SCHEME_MIGRATIONS.len();
+
+pub fn initialize(pool: SqlPool) -> MigrationResult {
+    println!("[ DB ] Initialize");
+
+    if let Ok(mut con) = pool.get() {
+        // Get the current database scheme version
+        // `PRAGMA user_version` is a SQLite specific variable for application use
         if let Ok(version) = con.query_row("PRAGMA user_version;", params!(), |r| {
             r.get::<usize, u32>(0)
         }) {
-            match version {
-                0 => first_setup(con),
-                // Add migrations here when needed
-                LATEST_SCHEME_VERSION => Ok(()),
-                _ => panic!(
+            let version = version as usize;
+
+            // Make sure we don't have a db from future
+            if version > SCHEME_VERSION {
+                panic!(
                     "Unknown scheme version: {} \n latest known version is: {}",
-                    version, LATEST_SCHEME_VERSION
-                ),
+                    version, SCHEME_VERSION
+                )
             }
+
+            // Execute migrations, if any
+            for migrate_to in version..SCHEME_VERSION {
+                // Create transaction
+                let tran = match con.transaction() {
+                    Ok(v) => v,
+                    Err(e) => {
+                        return Err(format!("Failed to create transaction with error: {}", e))
+                    }
+                };
+
+                // Execute migration
+                if let Err(e) = SCHEME_MIGRATIONS[migrate_to](&tran) {
+                    println!(
+                        "[ DB ] Failed migration v{} -> v{}",
+                        migrate_to,
+                        migrate_to + 1
+                    );
+
+                    return Err(e);
+                };
+
+                // Set the database version
+                if let Err(e) = tran.pragma_update(None, "user_version", &(migrate_to as u32 + 1)) {
+                    return Err(e.to_string());
+                }
+
+                // Commit transaction
+                if let Err(e) = tran.commit() {
+                    return Err(e.to_string());
+                }
+
+                println!("[ DB ] Migrate v{} -> v{}", migrate_to, migrate_to + 1);
+            }
+
+            Ok(())
         } else {
             Err("Failed to get database scheme version".to_owned())
         }
     } else {
         Err("Failed to get database connection".to_owned())
-    }
-}
-
-fn first_setup(mut con: Connection) -> Result<(), String> {
-    let tran = match con.transaction() {
-        Ok(v) => v,
-        Err(e) => return Err(format!("Failed to create transaction with error: {}", e)),
-    };
-
-    type Params = &'static [&'static dyn rusqlite::ToSql];
-    type Query = &'static str;
-
-    const TABLE_CREATE_SCRIPTS: [Query; 3] = [
-        "create table users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            email TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL,
-            subscription_expiration DATE
-        );",
-        "create table tokens (
-            token TEXT UNIQUE PRIMARY KEY,
-            user INTEGER NOT NULL,
-            expiration DATE NOT NULL,
-            FOREIGN KEY(user) REFERENCES users(id)
-        );",
-        "create table stripe_profiles (
-            stripe_id TEXT UNIQUE PRIMARY KEY,
-            user INTEGER UNIQUE NOT NULL,
-            stripe_subscription_expiration DATE,
-            stripe_subscription_start DATE,
-            last_updated DATE NOT NULL,
-            FOREIGN KEY(user) REFERENCES users(id)
-        );",
-    ];
-
-    const EMPTY_PARAMS: Params = &[];
-
-    for script in TABLE_CREATE_SCRIPTS.iter() {
-        if let Err(e) = tran.execute(script, EMPTY_PARAMS) {
-            return Err(e.to_string());
-        }
-    }
-    // We set the database version
-    if let Err(e) = tran.pragma_update(None, "user_version", &LATEST_SCHEME_VERSION) {
-        return Err(e.to_string());
-    }
-
-    match tran.commit() {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e.to_string()),
     }
 }
