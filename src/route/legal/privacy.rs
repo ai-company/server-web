c63a82a22dbfbf597f4fe3120f30fad7f24d66e2
Daniels Kursits <daniels.kursits@gmail.com>
2022-01-29 23:09:21 +0100

    Implement privacy policy

---
+++ src/route/legal/privacy.rs
@@ -0,0 +1,9 @@
+use actix_web::{web::Data, HttpResponse, Responder};
+
+use handlebars::Handlebars;
+
+use crate::middleware;
+
+pub async fn get(template: Data<Handlebars<'_>>, session: middleware::Session) -> impl Responder {
+    HttpResponse::Ok().body(template.render("page/legal/privacy", &session).unwrap())
+}
