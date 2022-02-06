50ba14fc8371a1f1eca656ddd2030dc22f9fbc97
Daniels Kursits <daniels.kursits@gmail.com>
2022-02-06 14:02:36 +0100

    Actually send account deletion email

--- src/route/user/delete.rs
+++ src/route/user/delete.rs
@@ -1,28 +1,32 @@
-
 use actix_web::http::header;
 use actix_web::{web, HttpRequest};
 use actix_web::{web::Data, HttpResponse, Responder};
-
 use handlebars::Handlebars;
 
-
-
-
 use crate::database::{user, SqlPool, SyncTransaction};
-use crate::helper::{get_current_user};
+use crate::emails::{account::deletion::AccountDeletionEmail, Email, Envelope};
+use crate::helper::get_current_user;
 use crate::middleware;
 use crate::route::{EndpointProcessingError, EndpointProcessingResult};
 
-
-
-async fn delete(req: HttpRequest, pool: &SqlPool) -> EndpointProcessingResult<()> {
+async fn delete(
+    req: HttpRequest,
+    pool: &SqlPool,
+    renderer: &Handlebars<'_>,
+) -> EndpointProcessingResult<()> {
     let user = get_current_user(&req, pool)?;
     let trans = SyncTransaction::new(pool)?;
+    let email = user.email.clone();
 
     user::delete(user, &trans).await?;
 
     trans
         .commit()
+        .map_err(|e| EndpointProcessingError::Processing(Box::new(e)))?;
+
+    AccountDeletionEmail::new()
+        .render(renderer)
+        .send(&email)
         .map_err(|e| EndpointProcessingError::Processing(Box::new(e)))?;
 
     Ok(())
@@ -38,7 +42,7 @@ pub async fn get(
 ) -> impl Responder {
     use EndpointProcessingError::*;
 
-    match delete(req, pool.as_ref()).await {
+    match delete(req, pool.as_ref(), &template).await {
         Ok(()) => HttpResponse::Ok().body(template.render("page/user/thankyou", &()).unwrap()),
         Err(Unauthorized) => HttpResponse::Found().header(header::LOCATION, "/").finish(),
         Err(RequestUnparsable) | Err(RequestNonsensical) => HttpResponse::BadRequest().finish(),
