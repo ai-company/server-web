5c4091539a21875c9e4f276ba8b6a12435ee6b4e
Daniels Kursits <daniels.kursits@gmail.com>
2022-02-11 16:22:55 +0100

    Reimplement correction reports

---
+++ src/database/correction_reports.rs
@@ -0,0 +1,17 @@
+use rusqlite::params;
+
+use super::{user::UserID, SyncTransaction};
+use super::{DBConnection, SqlPool};
+
+pub fn insert<C: DBConnection>(
+    con: C,
+    email: &str,
+    report: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    println!("[ DB ] Insert correction report");
+    con.execute(
+        "INSERT INTO correction_reports (email, report) VALUES (?1, ?2)",
+        params![email, report],
+    )?;
+    Ok(())
+}
